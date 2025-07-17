896e10eb70da8966d5e2282eea6e8927a9ff7dd7
Emerson Souza <140563230+EmersonDba84@users.noreply.github.com>
2025-07-17 20:24:59 -0300

    commit

---
+++ src/main/java/com/cadastramento/radiador/RadiadorApplication.java
@@ -0,0 +1,13 @@
+package com.cadastramento.radiador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadiadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadiadorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cadastramento/radiador/RadiadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cadastramento.radiador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadiadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
