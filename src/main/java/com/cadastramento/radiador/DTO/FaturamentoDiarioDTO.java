1d967f289e30c88bd505540c5314f8b4513d74f8
Emerson Souza <140563230+EmersonDba84@users.noreply.github.com>
2025-10-25 13:55:31 -0300

    commit
    migração para postgres

--- src/main/java/com/cadastramento/radiador/DTO/FaturamentoDiarioDTO.java
+++ src/main/java/com/cadastramento/radiador/DTO/FaturamentoDiarioDTO.java
@@ -3,16 +3,13 @@ package com.cadastramento.radiador.DTO;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-/**
- * DTO to carry revenue data grouped by day.
- */
 public class FaturamentoDiarioDTO {
     private final LocalDate data;
     private final BigDecimal total;
 
     public FaturamentoDiarioDTO(LocalDate data, BigDecimal total) {
         this.data = data;
-        // Ensures that if the sum is null (no services on a day), the total is 0.
+
         this.total = (total != null) ? total : BigDecimal.ZERO;
     }
 
